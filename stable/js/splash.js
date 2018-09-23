39ad91899245344b793acdf3e2cdde4bd0937677
Xola dos Santos <xdossantos@juta.co.za>
2018-09-23 23:51:27 +0200

    Adding music to the stable folder

--- stable/js/splash.js
+++ stable/js/splash.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
-import splash from '../assets/splash.png'
+import splash from '../assets/splash.png';
+import intromusic from '../assets/intro.mp3';
 
 export default class Splash extends Phaser.Scene {
     constructor() {
@@ -8,13 +9,15 @@ export default class Splash extends Phaser.Scene {
 
     preload() {
         this.load.image('splash', splash );
+        this.load.audio('intromusic', intromusic);
 
     }
 
 
     create() {
         this.add.image(400, 300, 'splash');
-     
+        let introductionmusic = this.sound.add('intromusic')
+        introductionmusic.play(); 
         this.ENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
     }
 
