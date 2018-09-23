2787bc0702564e62cf3b3ec696cbf281f8d0f296
Ntokozo Zwane <ntkzwane@gmail.com>
2018-09-23 23:00:50 +0200

    Added attack stuff

--- stable/js/game.js
+++ stable/js/game.js
@@ -15,6 +15,11 @@ var cursors;
 var score = 0;
 var gameOver = false;
 var scoreText;
+//enemy is temporary
+var enemy;
+var Vx;
+var lastShot = 0;
+var bullets;
 
 export default class Main extends Phaser.Scene {
   constructor() {
@@ -33,14 +38,15 @@ export default class Main extends Phaser.Scene {
   create () {
     //  A simple background for our game
     this.add.image(400, 300, 'sky');
+    this.add.image(1200, 300, 'sky');
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = this.physics.add.staticGroup();
 
     //  Here we create the ground.
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
+    platforms.create(800, 600, 'ground').setScale(4).refreshBody();
+    
     //  Now let's create some ledges
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
@@ -85,9 +91,74 @@ export default class Main extends Phaser.Scene {
       frames: [ { key: 'dudeSword', frame: 3 } ],
       frameRate: 10,
     });
+    
+    //this is temp to test the projectiles
+    enemy = this.physics.add.sprite(400, 512, 'dude');
+
+    enemy.setBounce(0);
+    enemy.setCollideWorldBounds(true);
+
+    //bullets,  projectiles
+      var Bullet = new Phaser.Class({
+      Extends: Phaser.GameObjects.Image,
+
+      initialize: 
+
+      function Bullet(scene) {
+        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'star');
+        this.speed = Phaser.Math.GetSpeed(400, 1);
+      }, 
+
+      fire: function(x, y, playerVelocity, lastShot) {
+        this.setPosition(x, y);
+        //if ((Phaser.time - 1) > lastShot) {
+          if (playerVelocity < 0) {
+            this.speed = Phaser.Math.GetSpeed(-400, 1);
+          }
+          else {
+            this.speed = Phaser.Math.GetSpeed(400, 1);  
+          };
+          this.setActive(true);
+          this.setVisible(true);
+        //}
+      },
+
+      update: function(time, delta) {
+        this.x += this.speed * delta;
+
+        if ((this.x >  800) || (this.x < 0)) {
+          this.setActive(false);
+          this.setVisible(false);
+        }
+        //x2:1 < x1:1 < x2:2 or x2:1 < x1:2 < x2:2 and
+        //y2:1 < y1:1 < y2:2 or y2:1 < y1:2 < y2:2 
+
+        if ((((enemy.x < this.x) && (this.x < (enemy.x + enemy.width))) || ((enemy.x < (this.x + this.width)) && ((this.x + this.width) < (enemy.x + enemy.width)))) 
+          && 
+            (((enemy.y < this.y) && (this.y < (enemy.y + enemy.height))) || ((enemy.y < (this.y + this.height)) && ((this.y + this.height) < (enemy.y + enemy.height))))) {
+          this.setActive(false);
+          this.setVisible(false);
+        }
+      }
+    });
+
+    bullets = this.add.group({
+         classType: Bullet,
+         maxSize: 10,
+         runChildUpdate: true
+    });
 
     //  Input Events
     cursors = this.input.keyboard.createCursorKeys();
+
+    //var cam = this.cameras.add(0, 0, 1600, 600);
+    //cam.setBounds(0, 0, 3200, 600);
+    //cam.startFollow(player);
+
+    //this.cameras.add(0, 0, 1600, 600);
+    this.cameras.main.setBounds(0, 0, 3200, 600);
+    this.cameras.main.startFollow(player); 
+    this.cameras.main.setSize(1200, 600);    
 
     //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
     stars = this.physics.add.group({
@@ -108,10 +179,13 @@ export default class Main extends Phaser.Scene {
     //  The score
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
+
     //  Collide the player and the stars with the platforms
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(stars, platforms);
     this.physics.add.collider(bombs, platforms);
+    //temp
+    this.physics.add.collider(enemy, platforms);  
 
     //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
     this.physics.add.overlap(player, stars, this.collectStar, null, this);
@@ -119,7 +193,7 @@ export default class Main extends Phaser.Scene {
 
 
     this.A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-
+    this.Shoot =  this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
   }
 
   update () {
@@ -131,7 +205,7 @@ export default class Main extends Phaser.Scene {
     if (cursors.left.isDown)
     {
       player.setVelocityX(-160);
-
+      Vx = -1;
       if (this.A.isDown) {
         player.anims.play('melee_left');
       } else {
@@ -142,7 +216,7 @@ export default class Main extends Phaser.Scene {
     else if (cursors.right.isDown)
     {
       player.setVelocityX(160);
-
+      Vx = 1;
       if (this.A.isDown) {
         player.anims.play('melee_right');
       } else {
@@ -160,6 +234,15 @@ export default class Main extends Phaser.Scene {
     {
       player.setVelocityY(-330);
     }
+
+    if (this.Shoot.isDown) {
+      var bullet = bullets.get();
+
+      if (bullet) {
+        bullet.fire(player.x, player.y, Vx, lastShot);
+        //lastShot = Phaser.time; 
+      }
+    }    
   }
 
   collectStar (player, star)
